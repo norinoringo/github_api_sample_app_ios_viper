126b34e6cc45b4c10729f69f6fcf69bf2f4fa893
HSaito <hisanoridevelop@gmail.com>
2022-04-23 08:15:51 +0900

    FetchGitHubRepositoryListUseCaseTestを実装した refs #18

---
+++ github_api_sample_app_ios_viperTests/UseCaseTest/FetchGitHubRepositoryListUseCaseTest.swift
@@ -0,0 +1,44 @@
+@testable import github_api_sample_app_ios_viper
+import XCTest
+
+class FetchGitHubRepositoryListUseCaseTest: XCTestCase {
+    var stubAPIClient: StubAPIClient!
+    var fetchGitHubRepositoryListUseCase: FetchGitHubRepositoryListUseCase!
+
+    override func setUp() {
+        super.setUp()
+        stubAPIClient = StubAPIClient()
+        fetchGitHubRepositoryListUseCase = FetchGitHubRepositoryListUseCase(repository: GitHubAPIClient(httpClient: stubAPIClient))
+    }
+
+    private func makeHTTPClientResult(statusCode: Int, json: String) -> Result<(Data, HTTPURLResponse), Error> {
+        return .success((
+            json.data(using: .utf8)!,
+            HTTPURLResponse(url: URL(string: "https://api.github.com")!,
+                            statusCode: statusCode,
+                            httpVersion: nil,
+                            headerFields: nil)!
+        ))
+    }
+}
+
+extension FetchGitHubRepositoryListUseCaseTest {
+    func testFetchGitHubRepository() {
+        let apiExpectaion = expectation(description: "wait for finish")
+        stubAPIClient.result = makeHTTPClientResult(statusCode: 200,
+                                                    json: GitHubRepositoryTest.exampleJson)
+        let input = FetchGitHubRepositoryListUseCaseInput(searchKeyword: "test")
+        fetchGitHubRepositoryListUseCase.fetchGitHubRepository(input: input).then { result in
+            let repository = result.githubRepository.items.first
+            XCTAssertEqual(repository?.repositoryName, "Backbone")
+            XCTAssertEqual(repository?.language, "JavaScript")
+            XCTAssertEqual(repository?.stargazersCount, 213)
+            XCTAssertEqual(repository?.description, "Backbone ドキュメント日本語訳")
+            apiExpectaion.fulfill()
+        }.catch { error in
+            XCTFail("unexpected result:\(error)")
+            apiExpectaion.fulfill()
+        }
+        wait(for: [apiExpectaion], timeout: 3)
+    }
+}
